ef285128ab527a1de6f9419f7f485d3fad3808c8
ThatAstronautGuy <jaredksemail@gmail.com>
2015-08-30 21:36:47 -0400

    Create about.php

---
+++ about.php
@@ -0,0 +1,30 @@
+<?php
+function customPageHeader(){?>
+  <!--Arbitrary HTML Tags-->
+<?php }
+
+include_once('./includes/header.phtml');
+?>
+<body>
+	<div id="contents">
+		<div class="wrapper clearfix">
+			<div class="main">
+				<h1>About Us</h1>
+				<p align="left">The Ottawa incarnation of Spacesim, from which the OCESS is descended, has existed for over 25 years, but unfortunately, there are many blanks in the current knowledge of Spacesim's history due to lack of record-keeping. Spacesim has done 26 missions to date.
+				<br/>	    
+				<a href="http://www.wiki.spacesim.org/index.php/Sim_History">Here</a> is the page where you can view all of the missions that the Ottawa chapter of Spacesim has conducted and participated in.
+				<br/>
+				<br/>				
+				The Ottawa chapter of Spacesim was created in 1988 by <a href="http://wiki.spacesim.org/index.php/Terry_Pritchett">Terry Pritchett.</a> He was a science teacher at <a href="http://lisgar.ca">Lisgar Collegiate Institute</a> for many years and was the Admiral at Space Sim for 8 years. In 1995 he won the <a href="http://www.ic.gc.ca/eic/site/pmate-ppmee.nsf/eng/wz01208.html">Prime Minister's Award for Teaching Excellence</a> for his involvement in the Spacesim program.
+				<br/>
+				<br/>
+				After Terry Pritchett retired and left Spacesim, <a href="http://www.wiki.spacesim.org/index.php/Maureen_McLean">Maureen McLean</a> became the new Admiral. She was the Admiral for one year before she was replaced by <a hreaf="http://www.wiki.spacesim.org/index.php/Craig_Hussey">Craig Hussey.</a> Four years later, <a href="http://www.wiki.spacesim.org/index.php/James_Magwood"> Jim Magwood</a> replaced him, and he has been the Admiral from 2001-Present.
+				<br/>
+				<br/>
+				Spacesim has had three <a hreaf="http://www.wiki.spacesim.org/index.php/Habitat">habitats,</a> or, habs to date. The hab is where the astronauts are for our 5 day long simulated space mission. In 2013-14 the mission was run a little differentially however, in that the launch for the mission was 1 month before the actual mission because the astronauts were going into "hibernation" for the 44 1/2 year journey to Alpha Centaurie B Beta.
+			</div>
+		</div>
+	</div>
+	<?php
+include_once('./includes/footer.phtml');
+?>
