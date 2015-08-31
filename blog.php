fbdc8a2385be03fe3ba2b92fd97d784afc34e722
ThatAstronautGuy <jaredksemail@gmail.com>
2015-08-30 21:37:15 -0400

    Create blog.php

---
+++ blog.php
@@ -0,0 +1,28 @@
+<?php
+function customPageHeader(){?>
+  <!--Arbitrary HTML Tags-->
+<?php }
+
+include_once('./includes/header.phtml');
+?>
+<body>
+		<div id="contents">
+		<div class="wrapper clearfix">
+			<div class="main">
+				<h1>Blog</h1>
+				<ul class="list">
+					<li>
+						<span class="time">03-11-2014</span>
+						<h4>Oh boy, the test website</h4>
+						<p>
+							So this is what the new spacesim website looks like so far. If anyone would like to give feedback to me the contact us form does work and it sends me an email, so you reach me with any suggestions, concerns, or content that is needed. (Like more stuff for the EEPs page, it is really empty) And yes, I do know that the download page does not work. I am currently setting it up. Also, if someone would like to make a logo that blends in with the website, then please do so, be it a gif or just a regular image. Thanks all :)
+						</p>
+					</li>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+<?php
+include_once('./includes/footer.phtml');
+?>
