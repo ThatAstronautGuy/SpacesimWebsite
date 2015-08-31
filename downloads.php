af0f97d70f53170091200ac5fb0e73155d855c9a
ThatAstronautGuy <jaredksemail@gmail.com>
2015-08-30 21:38:11 -0400

    Create downloads.php

---
+++ downloads.php
@@ -0,0 +1,22 @@
+<?php
+function customPageHeader(){?>
+  <!--Arbitrary HTML Tags-->
+<?php }
+
+include_once('./includes/header.phtml');
+?>
+  <body>
+  	<div id="contents">
+		 <div class="wrapper clearfix">
+		 	  <div class="main">
+    		  	   <h1>Downloads</h1>
+  				   <h2>Maps to Albert Street and map of Albert</h2>
+  				   <p>
+  				   	   	  <a href="downloads/map1.gif">Map to 440 from Lisgar</a>
+						  <br/>
+						  	   <a href="downloads/albert_street.jpg">Map of 440</a>
+						  <br/>	   
+				   </p>	  
+	<?php
+include_once('./includes/footer.phtml');
+?>
