19e7fb9144ebcc8444d9b663447c6ccc3c76b3cc
ThatAstronautGuy <jaredksemail@gmail.com>
2015-08-30 21:40:10 -0400

    Create legal.php

---
+++ legal.php
@@ -0,0 +1,62 @@
+<?php
+function customPageHeader(){?>
+  <!--Arbitrary HTML Tags-->
+<?php }
+
+include_once('./includes/header.phtml');
+?>
+  <body>
+  	<div id="contents">
+		 <div class="main">
+		 	  <h1>Legal</h1>
+		 </div>
+		 <div class="body clearfix">
+		 	  <p>
+			  	  By accessing the Spacesim website, you agree to the following terms:
+			  </p>	  
+			  <li>
+			  	  The contents of the Spacesim website are Copyright OCESS 2014. Any rights not expressly granted herein are reserved. Reproduction, transfer, distribution or storage of part or all of the contents in any form without the prior permission of OCESS is prohibited, unless such action is for personal use only.
+			  </li>
+			  <li>
+			  	  This site and the contents herein are provided as a convenience to you. The contents of the Spacesim website are provided on an "as is" and "as available" basis. Spacesim does not warrant that its webpages will be uninterrupted or error-free. OCESS reserves the right to revise the pages or withdraw access to them at any time.
+			  </li>
+			  <li>
+			  	 For your easy accessibility, Spacesim may include links to sites on the Internet that are owned or operated by third parties. You agree that Spacesim has no control over the content of those sites and cannot assume any responsibility for material created or published by such third-party sites.
+			  </li>
+			  <p>
+			  </p>
+			  <p>
+			  	 All pictures, videos, and animation in the Spacesim website are reprinted by permission only. All multimedia files are specifically tailored for use in the Spacesim website and may not be used elsewhere without permission.
+			  </p>
+			  <p>
+			  	 The Spacesim logo is copyright of Ross Green, and may not be used under any circumstances without his express written permission. 	 	 	  
+			  </p>
+		 </div>
+    </div>	  
+	<div id="footer">
+		<ul id="featured" class="wrapper clearfix">	
+			<li>
+				<img src="images/constellation_map.jpeg" alt="Img" height="204" width="220">
+				<h3><a href="eeps.html">EEPs</a></h3>
+				<p>
+					EEPs, or external education program is where we, spacesim go out into the public and educate people about space. Whether we are doing a planetarium presentation or something else we are always happy to teach!
+				</p>
+			</li>
+			<li>
+				<img src="images/stars_blog.jpg" alt="Img" height="204" width="220">
+				<h3><a href="blog.html">Updates</a></h3>
+				<p>
+					Find out whats going on this week, or see any updates on upcoming activities and events.
+				</p>
+			</li>
+			<li>
+				<img src="images/stars_eeps.jpg" alt="Img" height="204" width="220">
+				<h3><a href="contact.html">Contact Us</a></h3>
+				<p>
+					Have questions about us? Interested in planetarium presentations? Any questions about space in general? Here is where to reach us for anything you might need.
+				</p>
+			</li>
+		</ul>
+	<?php
+include_once('./includes/footer.phtml');
+?>
