bda567f75dfac45a032fd4b50296110c9c5e6b27
spencereir <spencer.whitehead@rogers.com>
2015-08-31 20:11:49 -0400

    So much

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
 	//Set up local variables
 	var collapsed = false;				//Handle collapsing menu
+	var menuOpen = false;				//Trac status of menu
+
+	//Force the page to start at top. This fixes menu problems
+ 	$(this).scrollTop(0);
 
 	//Load elements into the page. Load is async,
 	//so we then attach the things being loaded as a callback
-	$('#header').load('/includes/header.html', function() {
+	$('#header').load('/includes/header.html');
+	$('#menu-container').load('/includes/menu.html', function() {
 		$('#openMenu').click(function() {
+			menuOpen = true;
+			$('#openMenu').show().fadeOut(200);
 	  		$('#menu').animate({
 		        left: '0px'
 	      	}, 400);
@@ -13,36 +20,35 @@ $(document).ready(function() {
 	        	left: '300px'
 	    	}, 400);
 	  	});
-	});
-	$('#menu').load('/includes/menu.html', function() {
 		$('#closeMenu').click(function() {
+			menuOpen = false;
       		$('#menu').animate({
         		left: '-300px'
       		}, 400);
       		$('body').animate({
         		left: '0px'
       		}, 400);
+      		$('#openMenu').animate({
+      			left: '0px'
+      		}, 100);
+      		$('#openMenu').hide().fadeIn(300);
+      		//The animation delays here are touchy, send me a message if you think I should adjust them a bit
   		});
 	});
 	$('#footer').load('/includes/footer.html');
 
-	//Set up initial element visability states
-	$('#openMenu').hide();
-
 	$(window).scroll(function() {
-		if (collapsed === false && window.pageYOffset > 40) {
+		if (collapsed === false && window.pageYOffset > 120) {
 			collapsed = true;
-			$('#openMenu').hide().fadeIn(200);
-			$('#header').animate({
-				width: '8%'
-			}, 500);
+			$('#openMenu').animate({
+				top: '0px'
+			}, 200);
 		}
-		else if (collapsed === true && window.pageYOffset <= 40) {
+		else if (collapsed === true && window.pageYOffset <= 120) {
 			collapsed = false;
-			$('#openMenu').show().fadeOut(200);
-			$('#header').animate({
-				width: '100%'
-			}, 500)
+			$('#openMenu').animate({
+				top: '-40px'
+			}, 200);
 		}
 	});
 });
