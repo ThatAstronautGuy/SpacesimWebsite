a20d1c826d111357e365f0648f9f37ae6fb71eeb
spencereir <spencer.whitehead@rogers.com>
2015-09-04 11:50:21 -0400

    Autoscrolling carousel

--- js/main.js
+++ js/main.js
@@ -1,51 +1,96 @@
+function nextSlide() {
+	var currentSlide = $('.active-slide');
+	var nextSlide = currentSlide.next();
+
+	var currentDot = $('.active-dot');
+	var nextDot = currentDot.next();
+
+	if(nextSlide.length === 0) {
+		nextSlide = $('.slide').first();
+		nextDot = $('.dot').first();
+	}
+		
+	currentSlide.fadeOut(600).removeClass('active-slide');
+	nextSlide.fadeIn(600).addClass('active-slide');
+
+	currentDot.removeClass('active-dot');
+	nextDot.addClass('active-dot');
+}
+
+function prevSlide() {
+	var currentSlide = $('.active-slide');
+	var prevSlide = currentSlide.prev();
+
+	var currentDot = $('.active-dot');
+	var prevDot = currentDot.prev();
+
+	if(prevSlide.length === 0) {
+		prevSlide = $('.slide').last();
+		prevDot = $('.dot').last();
+	}
+		
+	currentSlide.fadeOut(600).removeClass('active-slide');
+	prevSlide.fadeIn(600).addClass('active-slide');
+
+	currentDot.removeClass('active-dot');
+	prevDot.addClass('active-dot');
+}
+
+function openMenu() {
+	$('#openMenu').show().fadeOut(200);
+	$('#menu').animate({
+		left: '0px'
+	}, 400);
+	$('body').animate({
+		left: '300px'
+	}, 400);
+}
+
+function closeMenu() {
+	$('#menu').animate({
+		left: '-300px'
+	}, 400);
+	$('body').animate({
+		left: '0px'
+	}, 400);
+	$('#openMenu').animate({
+		left: '0px'
+	}, 100);
+	$('#openMenu').hide().fadeIn(300);
+}
+
 $(document).ready(function() {
 	//Set up local variables
 	var collapsed = false;				//Handle collapsing menu
-	var menuOpen = false;				//Trac status of menu
 
 	//Force the page to start at top. This fixes menu problems
- 	$(window).scrollTop(0);
+	$(window).scrollTop(0);
 
- 	var active = window.location.pathname;
- 	var current_active_class = '#index';
+	var active = window.location.pathname;
+	var current_active_class = '#index';
+
+	var interval = setInterval(nextSlide, 4000);
 
 	//Load elements into the page. Load is async,
 	//so we then attach the things being loaded as a callback
 	$('#header').load('/includes/header.html', function() {
-  		$('#navigation a').each(function() {							//This works, but I feel terrible
-  			if (active === "/" + $(this).attr('href') || (active === "/" && $(this).attr('href') === "index.html")) {
+		$('#navigation a').each(function() {							//This works, but I feel terrible
+			if (active === "/" + $(this).attr('href') || (active === "/" && $(this).attr('href') === "index.html")) {
 					$(this).parent().addClass('active');
-  				$(current_active_class).parent().removeClass('active');
-  				current_active_class = ("#" + $(this).attr('href')).substring(0, ("#" + $(this).attr('href')).length - 5);
-  				console.log(current_active_class);
-  			}
-  		});
+				$(current_active_class).parent().removeClass('active');
+				current_active_class = ("#" + $(this).attr('href')).substring(0, ("#" + $(this).attr('href')).length - 5);
+				console.log(current_active_class);
+			}
+		});
 	});
+
 	$('#menu-container').load('/includes/menu.html', function() {
 		$('#openMenu').click(function() {
-			menuOpen = true;
-			$('#openMenu').show().fadeOut(200);
-	  		$('#menu').animate({
-		        left: '0px'
-	      	}, 400);
-	      	$('body').animate({
-	        	left: '300px'
-	    	}, 400);
-	  	});
+			openMenu();
+		});
 		$('#closeMenu').click(function() {
-			menuOpen = false;
-      		$('#menu').animate({
-        		left: '-300px'
-      		}, 400);
-      		$('body').animate({
-        		left: '0px'
-      		}, 400);
-      		$('#openMenu').animate({
-      			left: '0px'
-      		}, 100);
-      		$('#openMenu').hide().fadeIn(300);
-      		//The animation delays here are touchy, send me a message if you think I should adjust them a bit
-  		});
+			closeMenu();
+		});
 	});
 	$('#footer').load('/includes/footer.html');
 
@@ -68,40 +113,14 @@ $(document).ready(function() {
 	
 	//Code to handle the carousel
 	$('#arrow-next').click(function() {
-	    var currentSlide = $('.active-slide');
-	    var nextSlide = currentSlide.next();
-
-	    var currentDot = $('.active-dot');
-	    var nextDot = currentDot.next();
-
-	    if(nextSlide.length === 0) {
-	      nextSlide = $('.slide').first();
-	      nextDot = $('.dot').first();
-	    }
-	    
-	    currentSlide.fadeOut(600).removeClass('active-slide');
-	    nextSlide.fadeIn(600).addClass('active-slide');
-
-	    currentDot.removeClass('active-dot');
-		nextDot.addClass('active-dot');
+		nextSlide();
+		clearInterval(interval);
+		interval = setInterval(nextSlide, 4000);
 	});
 
 	$('#arrow-prev').click(function() {
-	    var currentSlide = $('.active-slide');
-	    var prevSlide = currentSlide.prev();
-
-	    var currentDot = $('.active-dot');
-	    var prevDot = currentDot.prev();
-
-	    if(prevSlide.length === 0) {
-	      prevSlide = $('.slide').last();
-	      prevDot = $('.dot').last();
-	    }
-	    
-	    currentSlide.fadeOut(600).removeClass('active-slide');
-	    prevSlide.fadeIn(600).addClass('active-slide');
-
-	    currentDot.removeClass('active-dot');
-	    prevDot.addClass('active-dot');
-  	});
+		prevSlide();
+		clearInterval(interval);
+		interval = setInterval(nextSlide, 4000);
+	});
 });
