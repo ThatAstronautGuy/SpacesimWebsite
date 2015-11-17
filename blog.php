be96058fa823ff5824a987aa6b733b7010c5aa0a
Ivan Fonseca <ivanfonseca55@gmail.com>
2015-11-16 22:38:47 -0500

    Newlines in blog files doesn't break it anymore

--- blog.php
+++ blog.php
@@ -64,8 +64,8 @@
                 foreach($posts as $val) {
                     $myfile = fopen("blogPosts/" . $val, "r") or die("Unable to open file!");
                     $postText = fread($myfile,filesize("blogPosts/" . $val));
-                    echo '$("#blogContainer").append("' .  $Parsedown->text("$postText") . '");
-        ';
+                    $postText = preg_replace("/\r\n|\r|\n/",'<br/>',$postText);
+                    echo "$('#blogContainer').append('" .  $Parsedown->text("$postText") . "')";
                     fclose($myfile);
                 }
                 
